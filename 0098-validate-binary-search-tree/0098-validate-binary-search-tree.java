485c492884f530e2197efbfab833f25a96e9b312
adarshsingh28604 <adarshsingh28604@gmail.com>
2026-06-21 16:44:43 +0530

    Time: 1 ms (15.31%), Space: 45.5 MB (13.44%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,31 +1,19 @@
+
 class Solution {
+      public void inorder(TreeNode root, ArrayList<Integer> ans){
+         if(root==null) return;
+         inorder(root.left,ans);
+         ans.add(root.val);
+         inorder(root.right,ans);
 
-    private long max(TreeNode root) {
-        if (root == null) return Long.MIN_VALUE;
 
-        long a = root.val;
-        long b = max(root.left);
-        long c = max(root.right);
-
-        return Math.max(a, Math.max(b, c));
-    }
-
-    private long min(TreeNode root) {
-        if (root == null) return Long.MAX_VALUE;
-
-        long a = root.val;
-        long b = min(root.left);
-        long c = min(root.right);
-
-        return Math.min(a, Math.min(b, c));
-    }
-
+     }
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-
-        if (root.val <= max(root.left)) return false;
-        if (root.val >= min(root.right)) return false;
-
-        return isValidBST(root.left) && isValidBST(root.right);
+         ArrayList<Integer> ans = new ArrayList<>();
+        inorder(root,ans);
+        for(int i = 1; i<ans.size();i++){
+            if(ans.get(i)<=ans.get(i-1)) return false;
+        }
+        return true;
     }
 }
