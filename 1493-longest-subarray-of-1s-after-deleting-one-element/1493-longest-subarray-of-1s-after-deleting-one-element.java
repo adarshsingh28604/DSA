4945184a77a410a674a0a89edb6b03745ce9f347
adarshsingh28604 <adarshsingh28604@gmail.com>
2026-07-24 19:42:26 +0530

    Time: 2 ms (99.29%), Space: 65.3 MB (67.87%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int longestSubarray(int[] arr) {
+        int n = arr.length , i = 0 , j = 0;
+
+        int z = 0;
+        for(int ele: arr){
+            if(ele==0) z++;
+        }
+        if(z==0) return n-1;
+
+        int zeroes = 0 , maxLen = 0;
+        while(i<n && arr[i]==0) i++;
+        if(i==n) return 0;
+        j=i;
+
+        while(j<n){
+            if(arr[j]==1) j++;
+            else {
+                if(zeroes==0){
+                    j++;
+                    zeroes++;
+                }
+                else {
+                   int len = j-i-1;
+                   maxLen = Math.max(maxLen , len);
+                   j++;
+                    while(i<n && arr[i]==1) i++;
+                    i++;
+                }
+            }
+        }
+        if(zeroes == 0) return j-i;
+         int len = j-i-1;
+         maxLen = Math.max(maxLen , len);
+        return maxLen;
+    }
+}
