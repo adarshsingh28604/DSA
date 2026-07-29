ef872b469454872786490f81d8dab047af18ccd9
Adarsh Singh <adarshsingh28604@gmail.com>
2026-07-30 04:42:24 +0530

    Time: 0 ms (100%), Space: 42.5 MB (88.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,14 @@
 class Solution {
+    public int amount(int[] arr, int i, int[] dp){
+        if(i>=arr.length) return 0;
+        if(dp[i]!=-1) return dp[i];
+      int take = arr[i] + amount(arr , i+2 , dp);
+      int skip = amount(arr,i+1,dp);
+      return dp[i] = Math.max(take,skip);
+    }
     public int rob(int[] arr) {
-        int n = arr.length;
-        int[] dp = new int[n];
-        dp[0]=arr[0];
-      if(n>1)  dp[1] = Math.max(arr[0],arr[1]);
-
-      for(int i = 2; i< n ; i++){
-        dp[i] = Math.max(arr[i]+dp[i-2],dp[i-1]);
-      }
-       return dp[n-1];
+        int[] dp = new int[arr.length];
+        Arrays.fill(dp,-1);
+        return amount(arr,0 , dp);
     }
 }
