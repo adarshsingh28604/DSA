48e81a1d6ae88f8ddf51ab5273630a80eb1c6b4f
Adarsh Singh <adarshsingh28604@gmail.com>
2026-08-05 01:16:08 +0530

    Time: 81 ms (30.92%), Space: 76.4 MB (8.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,44 @@
 class Solution {
-    public boolean subset(int i , int[] arr , int target , int[][] dp ){
-        if(i==arr.length){
-            if(target == 0) return true;
-            else return false;
-        }
-        if(dp[i][target]!=-1) return (dp[i][target]==1);
-        boolean ans = false;
-        boolean skip = subset(i+1,arr,target,dp);
-        if(target-arr[i]<0) ans = skip;
-        else {
-            boolean pick = subset(i+1, arr , target - arr[i] , dp);
-            ans = pick || skip;
-        }
-        dp[i][target]= (ans) ? 1: 0;
-        return ans;
-    }
-    public boolean canPartition(int[] nums) {
+    public boolean canPartition(int[] arr) {
+
         int sum = 0;
-        for(int ele: nums){
-           sum += ele;
+        int n = arr.length;
+
+        for (int x : arr) {
+            sum += x;
         }
-        if(sum%2!=0) return false;
+
+        if (sum % 2 != 0) return false;
+
         int target = sum / 2;
 
-        int[][] dp = new int [nums.length][target+1];
-        for (int i = 0; i < dp.length; i++) {
-         Arrays.fill(dp[i], -1);
-         }
-         return subset(0,nums, target , dp);
+        int[][] dp = new int[n][target + 1];
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j <= target; j++) {
+
+                boolean ans = false;
+
+                // Skip current element
+                boolean skip = (i > 0) ? (dp[i - 1][j] == 1) : (j == 0);
+
+                if (j - arr[i] < 0) {
+                    ans = skip;
+                } else {
+
+                    // Pick current element
+                    boolean pick = (i > 0)
+                            ? (dp[i - 1][j - arr[i]] == 1)
+                            : (j - arr[i] == 0);
+
+                    ans = pick || skip;
+                }
+
+                dp[i][j] = ans ? 1 : 0;
+            }
+        }
+
+        return dp[n - 1][target] == 1;
     }
 }
