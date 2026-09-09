52ecda0adc1668fc9bb0a2158fd991805fdf0f22
Adarsh Singh <adarshsingh28604@gmail.com>
2026-09-10 00:17:15 +0530

    Time: 1 ms (99.36%), Space: 42.8 MB (37.18%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -10,20 +10,16 @@ class Solution {
     public long countCommas(long n) {
        int digit =  digits(n);
        if(n<1000) return 0;
-       if(digit == 4 ) return ((n - 1000) + 1);
-       if(digit == 5 ) return ((n - 1000) + 1);
-       if(digit == 6 ) return ((n - 1000) + 1);
-       if(digit == 7 ) return ((n - 1000) + 1) + ((n - 1000000) + 1) ;
-       if(digit == 8 ) return ((n - 1000) + 1) + ((n - 1000000) + 1) ;
-       if(digit == 9 ) return ((n - 1000) + 1) + ((n - 1000000) + 1) ;
-       if(digit == 10 ) return ((n - 1000) + 1) + ((n - 1000000) + 1)  + ((n - 1000000000L) + 1);
-       if(digit == 11 ) return ((n - 1000) + 1) + ((n - 1000000) + 1)  + ((n - 1000000000L) + 1);
-       if(digit == 12 ) return ((n - 1000) + 1) + ((n - 1000000) + 1)  + ((n - 1000000000L) + 1);
-       if(digit == 13 ) return ((n - 1000) + 1) + ((n - 1000000) + 1)  + ((n - 1000000000L) + 1) + ((n - 1000000000000L) + 1);
-       if(digit == 14) return ((n - 1000) + 1) + ((n - 1000000) + 1)  + ((n - 1000000000L) + 1) + ((n - 1000000000000L) + 1);
-       if(digit == 15 ) return ((n - 1000) + 1) + ((n - 1000000) + 1)  + ((n - 1000000000L) + 1) + ((n - 1000000000000L) + 1);
+       if(digit == 4 || digit == 5 || digit == 6  ) return ((n - 1000) + 1);
+
+       if(digit == 7 || digit == 8 || digit == 9  ) return ((n - 1000) + 1) + ((n - 1000000) + 1) ;
+  
+       if(digit == 10 || digit == 11 || digit == 12  ) return ((n - 1000) + 1) + ((n - 1000000) + 1)  + ((n - 1000000000L) + 1);
+
+       if(digit == 13 || digit == 14 || digit == 15  ) return ((n - 1000) + 1) + ((n - 1000000) + 1)  + ((n - 1000000000L) + 1) + ((n -1000000000000L) + 1);
+
         if (digit == 16) return ((n - 1000) + 1) + ((n - 1000000) + 1) + ((n - 1000000000L) + 1) + ((n - 1000000000000L) + 1)
- + ((n - 1000000000000000L) + 1);
- return 0;
+        + ((n - 1000000000000000L) + 1);
+       return 0;
     }
 }
