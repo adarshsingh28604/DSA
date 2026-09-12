e646069c28e4952ae149d5afacc9d4db381c91b8
Adarsh Singh <adarshsingh28604@gmail.com>
2026-09-12 23:30:08 +0530

    Time: 4 ms (91.46%), Space: 47.1 MB (64.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,4 +1,17 @@
 class Solution {
+  Boolean ans = true;
+    public void dfs(int i , List<List<Integer>> adj ,boolean[] vis ,boolean[] path){
+        vis[i]=true;
+        path[i]=true;
+        for(int ele: adj.get(i)){
+            if(path[ele]==true){
+                ans=false;
+                return;
+            }
+            if(vis[ele]==false) dfs(ele,adj,vis,path);
+        }
+        path[i] = false;
+    }
 
     public boolean canFinish(int n, int[][] pre) {
 
@@ -7,35 +20,44 @@ class Solution {
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        int[] indegree = new int[n];
-        boolean[] visited = new boolean[n];
+        // int[] indegree = new int[n];
+        // boolean[] visited = new boolean[n];
         for(int i = 0; i < pre.length;i++){
             int a = pre[i][0] , b = pre[i][1];
-            adj.get(b).add(a);;
-            indegree[a]++;
+            adj.get(b).add(a);
+            // indegree[a]++;
         }
-        // kahn's algo
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> ans =  new ArrayList<>();
+        boolean[] vis = new boolean[n];
+        boolean[] path = new boolean[n];
 
-        for(int i = 0; i < n ; i++){
-            if(indegree[i] == 0){
-                q.add(i);
-                visited[i]=false;
-            }
-        }
-        while(q.size()>0){
-            int front = q.remove();
-            ans.add(front);
-            for(int ele: adj.get(front)){
-                indegree[ele]--;
-                if(indegree[ele]==0){
-                    q.add(ele);
-                    visited[ele] = true;
-                }
-            }
-        }
-      if(ans.size()==n) return true;
-      else return false;
+         for (int i = 0; i < n; i++) {
+            if(vis[i]==false) dfs(i,adj,vis,path);
+         }
+
+         return ans;
+
+    //     // kahn's algo
+    //     Queue<Integer> q = new LinkedList<>();
+    //     List<Integer> ans =  new ArrayList<>();
+
+    //     for(int i = 0; i < n ; i++){
+    //         if(indegree[i] == 0){
+    //             q.add(i);
+    //             visited[i]=false;
+    //         }
+    //     }
+    //     while(q.size()>0){
+    //         int front = q.remove();
+    //         ans.add(front);
+    //         for(int ele: adj.get(front)){
+    //             indegree[ele]--;
+    //             if(indegree[ele]==0){
+    //                 q.add(ele);
+    //                 visited[ele] = true;
+    //             }
+    //         }
+    //     }
+    //   if(ans.size()==n) return true;
+    //   else return false;
     }
 }
